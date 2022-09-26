4b16c7dceaecde855d36f36f98cc860a2e9054aa
IsraExe <israel-w@hotmail.com>
2022-09-26 00:26:53 -0300

    first commit

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import { useState }from 'react'
+import BlogList from './BlogList';
+
+const Home = () => {
+    const [ blogs, setBlogs ] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }    
+    ])
+
+    const handleDelete = (id) => {
+        console.log('This is working')
+    }
+
+    return ( 
+        <div className="home">
+            <BlogList blogs = {blogs} title="All blogs" />
+            <BlogList blogs = {blogs.filter((blog) => blog.author == 'mario')} title="All blogs" handleDelete={handleDelete} />
+        </div>
+     );
+}
+ 
+export default Home;
