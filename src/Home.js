57dfc8757fee51b4135f54f09a798a1b6581746b
IsraExe <israel-w@hotmail.com>
2022-09-27 09:06:29 -0300

    Functional delete button

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,29 @@
-import { useState }from 'react'
+import { useState, useEffect }from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [ blogs, setBlogs ] = useState([
+    const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }    
     ])
 
+    const [name, setName] = useState('Mario')
+
     const handleDelete = (id) => {
-        console.log('This is working')
+        const newBlog = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlog)
     }
+
+    useEffect(() => {
+        console.log('state of name changed')
+    }, [name])
 
     return ( 
         <div className="home">
-            <BlogList blogs = {blogs} title="All blogs" />
-            <BlogList blogs = {blogs.filter((blog) => blog.author == 'mario')} title="All blogs" handleDelete={handleDelete} />
+            <BlogList blogs = {blogs} title="All blogs" handleDelete={handleDelete}/>
+            <button onClick={() => setName('Luigi')}>Change Name</button>
+            <p>{name}</p>
         </div>
      );
 }
